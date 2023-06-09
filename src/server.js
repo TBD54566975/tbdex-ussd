f9c80017d465d487c4b392aa2ff90fed21cb8186
Mic Neale <michael.neale@gmail.com>
2023-06-09 17:15:13 +1000

    using ECMA

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
-const app = require('express')()
-const bodyParser = require('body-parser')
-const logger = require('morgan')
+import express from 'express'
+import bodyParser from 'body-parser'
+import logger from 'morgan'
 
+const app = express()
 const port = process.env.PORT || 3030
 
 app.use(logger('dev'))
@@ -15,31 +16,24 @@ app.get('*', (req, res) => {
 app.post('*', (req, res) => {
   let {sessionId, serviceCode, phoneNumber, text} = req.body
   if (text == '') {
-    // This is the first request. Note how we start the response with CON
     let response = `CON What would you want to check
     1. My Account
     2. My phone number`
     res.send(response)
   } else if (text == '1') {
-    // Business logic for first level response
     let response = `CON Choose account information you want to view
     1. Account number
     2. Account balance`
     res.send(response)
   } else if (text == '2') {
-    // Business logic for first level response
     let response = `END Your phone number is ${phoneNumber}`
     res.send(response)
   } else if (text == '1*1') {
-    // Business logic for first level response
     let accountNumber = 'ACC1001'
-    // This is a terminal request. Note how we start the response with END
     let response = `END Your account number is ${accountNumber}`
     res.send(response)
   } else if (text == '1*2') {
-    // This is a second level response where the user selected 1 in the first instance
     let balance = 'NGN 10,000'
-    // This is a terminal request. Note how we start the response with END
     let response = `END Your balance is ${balance}`
     res.send(response)
   } else {
