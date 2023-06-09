d56a5459e5fbc23cb92d68377fd865383f3b2483
Mic Neale <michael.neale@gmail.com>
2023-06-09 17:38:50 +1000

    now working a bit

--- src/server.js
+++ src/server.js
@@ -9,6 +9,12 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
+import { Web5 } from '@tbd54566975/web5';
+
+const { web5, did: serverDid } = await Web5.connect();
+
+
+
 app.get('*', (req, res) => {
   res.send('This is tutorial App on creating your first USSD app in 5 minutes or less by Ajala Abdulsamii <kgasta@gmail.com>')
 })
@@ -18,7 +24,8 @@ app.post('*', (req, res) => {
   if (text == '') {
     let response = `CON What would you want to check
     1. My Account
-    2. My phone number`
+    2. My phone number
+    3. My DID`
     res.send(response)
   } else if (text == '1') {
     let response = `CON Choose account information you want to view
@@ -27,6 +34,9 @@ app.post('*', (req, res) => {
     res.send(response)
   } else if (text == '2') {
     let response = `END Your phone number is ${phoneNumber}`
+    res.send(response)
+  } else if (text == '3') {
+    let response = `END Your DID is ${serverDid}`
     res.send(response)
   } else if (text == '1*1') {
     let accountNumber = 'ACC1001'
